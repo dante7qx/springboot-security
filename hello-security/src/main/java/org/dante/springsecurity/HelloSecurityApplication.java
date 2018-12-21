d05c6d895e69160ccc856e2d4331de02861f81b7
dante <sunchao.0129@163.com>
2018-12-21 10:40:01 +0800

    完善

--- hello-security/src/main/java/org/dante/springsecurity/HelloSecurityApplication.java
+++ hello-security/src/main/java/org/dante/springsecurity/HelloSecurityApplication.java
@@ -3,6 +3,12 @@ package org.dante.springsecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 第一个 Spring Security 应用
+ * 
+ * @author dante
+ *
+ */
 @SpringBootApplication
 public class HelloSecurityApplication {
 
